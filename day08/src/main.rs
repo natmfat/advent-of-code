f486b88b7d89b0301401fe6d6cb55d6ddfd3f02c
Nathan <146884027+natmfat@users.noreply.github.com>
2024-12-07 22:11:19 -0800

    feat: day08 p2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -3,24 +3,8 @@ use std::collections::{HashMap, HashSet};
 fn main() {
   let input = std::fs::read_to_string("./input.txt").expect("expected input data");
   let mut grid = Grid::new(input.lines().map(|line| line.chars().collect()).collect());
-  println!("part 1 = {}", grid.core());
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct Point(i32, i32);
-
-impl Point {
-  fn new(x: i32, y: i32) -> Point {
-    return Point(x, y);
-  }
-
-  fn add(point_a: &Point, point_b: &Point) -> Point {
-    return Point::new(point_a.0 + point_b.0, point_a.1 + point_b.1);
-  }
-
-  fn sub(point_a: &Point, point_b: &Point) -> Point {
-    return Point::new(point_a.0 - point_b.0, point_a.1 - point_b.1);
-  }
+  println!("part 1 = {}", grid.core(false));
+  println!("part 2 = {}", grid.core(true));
 }
 
 #[derive(Debug)]
@@ -32,12 +16,15 @@ struct Grid {
 
 impl Grid {
   fn new(values: Vec<Vec<char>>) -> Grid {
-    return Grid {
+    let mut grid = Grid {
       frequencies: HashMap::new(),
       values,
     };
+    grid.get_antennas();
+    return grid;
   }
 
+  #[allow(dead_code)]
   fn to_string(&self) -> String {
     return self
       .values
@@ -53,8 +40,8 @@ impl Grid {
       .join("\n");
   }
 
-  fn core(&mut self) -> i32 {
-    // get all antennas
+  /// get all antennas
+  fn get_antennas(&mut self) {
     for y in 0..self.height() {
       for x in 0..self.width() {
         let frequency = self
@@ -70,9 +57,10 @@ impl Grid {
         }
       }
     }
+  }
 
+  fn core(&mut self, all_line: bool) -> i32 {
     let mut used_positions: HashSet<Point> = HashSet::new();
-
     for (_, positions) in self.frequencies.iter() {
       for i in 0..(positions.len() - 1) {
         let pos_a = positions.get(i).expect("expected position at i");
@@ -81,18 +69,34 @@ impl Grid {
 
           // compute slope-ish (how line is changing, but we really want this as a point/vec)
           let vel = Point::sub(pos_b, pos_a);
-          let possible_antinodes = vec![Point::sub(pos_a, &vel), Point::add(pos_b, &vel)];
-
-          for antinode in possible_antinodes {
-            if self.point_within_bounds(&antinode) {
-              used_positions.insert(antinode.clone());
-              self.values[antinode.1 as usize][antinode.0 as usize] = '#';
+          // for part 2, get any antinode in the same path
+          if all_line {
+            // get upper left most point
+            let mut curr_pos = pos_a.clone();
+            while self.point_within_bounds(&curr_pos) {
+              curr_pos = Point::sub(&curr_pos, &vel);
+            }
+            curr_pos = Point::add(&curr_pos, &vel);
+            while self.point_within_bounds(&curr_pos) {
+              if self.point_within_bounds(&curr_pos) {
+                used_positions.insert(curr_pos.clone());
+                // if self.get(curr_pos.0, curr_pos.1) == Some('.') {
+                //   self.values[curr_pos.1 as usize][curr_pos.0 as usize] = '#';
+                // }
+              }
+              curr_pos = Point::add(&curr_pos, &vel);
+            }
+          } else {
+            let possible_antinodes = vec![Point::sub(pos_a, &vel), Point::add(pos_b, &vel)];
+            for antinode in possible_antinodes {
+              if self.point_within_bounds(&antinode) {
+                used_positions.insert(antinode.clone());
+              }
             }
           }
         }
       }
     }
-
     return used_positions.len() as i32;
   }
 
@@ -127,5 +131,22 @@ impl Grid {
 
   fn height(&self) -> i32 {
     return self.values.len().try_into().expect("parse usize into i32");
+  }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Point(i32, i32);
+
+impl Point {
+  fn new(x: i32, y: i32) -> Point {
+    return Point(x, y);
+  }
+
+  fn add(point_a: &Point, point_b: &Point) -> Point {
+    return Point::new(point_a.0 + point_b.0, point_a.1 + point_b.1);
+  }
+
+  fn sub(point_a: &Point, point_b: &Point) -> Point {
+    return Point::new(point_a.0 - point_b.0, point_a.1 - point_b.1);
   }
 }
